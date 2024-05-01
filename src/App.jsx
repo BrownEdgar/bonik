6f201858f030d59b190a3efa620b8c0385b2b5c8
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-05-01 19:03:13 +0400

    delivery done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import DeliverySection from './components/DeliverySection/DeliverySection'
 export default function App() {
   return (
     <div className='App'>
-
+      <DeliverySection />
     </div>
   )
 }
