bc6d88091b8412306d42b5afe7319f8726719055
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-05-02 00:40:57 +0400

    bag fix

--- src/components/Applications/Applications.jsx
+++ src/components/Applications/Applications.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Applications.scss";
+import ApplicationApp from "../AplicationApp/ApplicationApp";
 
 export default function Applications() {
   return (
@@ -11,46 +12,23 @@ export default function Applications() {
           goods from a source location to a predefined destination.
         </p>
         <div className="Applications__content-list">
-          <div className="Applications__content-list-one">
+          <div>
             <p>01</p>
             <p>Follow Delivery Status</p>
           </div>
-          <div className="Applications__content-list-two">
+          <div>
             <p>02</p>
             <p>Order From Any Location</p>
           </div>
-          <div className="Applications__content-list-three">
+          <div>
             <p>03</p>
             <p>Get Important Notices</p>
           </div>
         </div>
-
-        <div className="app">
-          <a href="https://www.apple.com/app-store/" target="_blank">
-            <div className="app__content">
-              <i className="bx bxl-apple"></i>
-              <div className="app__content-left">
-                <p>Download on the</p>
-                <h2>App Store</h2>
-              </div>
-            </div>
-          </a>
-          <a
-            href="https://play.google.com/store/games?hl=ru&gl=US&pli=1"
-            target="_blank"
-          >
-            <div className="app__content">
-              <img src="../icon/google.svg" alt="" />
-              <div className="app__content-right">
-                <p>Get it on</p>
-                <h2>Google Play</h2>
-              </div>
-            </div>
-          </a>
-        </div>
+        <ApplicationApp />
       </div>
       <div className="Applications__image">
-        <img src="../image/app_image.png" alt="app_image" />
+        <img src="../images/app_image.png" alt="app_image" />
       </div>
     </div>
   );
