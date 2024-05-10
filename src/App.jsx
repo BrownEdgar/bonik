d8d12fbfea04e39a2764c8ccd7c0457d00ff3a58
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-05-10 19:48:30 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-
 import React from "react";
+import Questions from "./components/Questions/Questions";
 import DeliverySection from './components/DeliverySection/DeliverySection'
 import Applications from "./components/Applications/Applications";
 import Footer from "./components/Footer/Footer";
@@ -7,6 +7,7 @@ import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
     <div className="App">
+      <Questions />
       <Applications />
       <DeliverySection />
       <Footer />
